dfb786f051cf53f22280239f37fbf14ee42ee066
DanielfLacerda <daniel.felacerda@hotmail.com>
2023-10-06 15:49:13 -0300

    Building adjustment

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ProProfile
+# danielflacerda.github.io
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.5.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ProProfile';
+  title = 'danielflacerda.github.io';
 }
