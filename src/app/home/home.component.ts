94cf94d84c09545c52db742d1e0b81ef862b81ca
DanielfLacerda <daniel.felacerda@hotmail.com>
2023-10-08 00:21:42 -0300

    Commiting progress on carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SlideInterface } from '../stackcarousel/types/slide.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  slides: SlideInterface[] = [
+    { url: '/assets/image-1.jpeg', title: 'beach' },
+    { url: '/assets/image-2.jpeg', title: 'boat' },
+    { url: '/assets/image-3.jpeg', title: 'forest' },
+    { url: '/assets/image-4.jpeg', title: 'city' },
+    { url: '/assets/image-5.jpeg', title: 'italy' },
+  ];
 
   constructor() { }
 
