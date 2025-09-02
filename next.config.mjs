10ce83f030d0752e68ee1c6a8503edc93527686b
danielflacerda <daniel.felacerda@hotmail.com>
2025-09-02 18:45:33 -0300

    [FIX] Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'export',
+  distDir: 'out',
   eslint: {
     ignoreDuringBuilds: true,
   },
